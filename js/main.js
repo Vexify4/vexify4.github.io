e62394d9c7d38b05455a30946424211c8174fa41
Vexify4 <26hanleylj@gmail.com>
2022-01-17 21:18:47 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function aboutFunction() {
+    var x = document.getElementById("aboutDIV");
+    var y = document.getElementById("social");
+    var z = document.getElementById("projects");
+    if (x.style.display === "none") {
+      x.style.display = "block"; }
+     else {
+      x.style.display = "none";
+    }
+  }
+
+function socialFunction() {
+    var x = document.getElementById("socialDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function projectFuction() {
+    var x = document.getElementById("projectDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
